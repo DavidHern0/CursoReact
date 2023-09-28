5c1a4479c3e8413030a3802cf4e04d218c956346
DavidHern0 <dhernandezla0@gmail.com>
2023-09-28 14:42:04 +0200

    Curso react #1

---
+++ projects/01-tic-tac-toe/src/logic/board.js
@@ -0,0 +1,24 @@
+import { WINNER_COMBOS } from "../constants";
+
+export const checkWinnerFrom = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+        const [a, b, c] = combo;
+        if (
+            boardToCheck[a] &&
+            boardToCheck[a] === boardToCheck[b] &&
+            boardToCheck[a] === boardToCheck[c]
+        ) {
+            return boardToCheck[a];
+        }
+    }
+    return null;
+}
+
+export const checkEndGame = (newBoard) => {
+    return newBoard.every((square) => square !== null);
+}
+
+export function removeFromLocalStorage () {
+    window.localStorage.removeItem('board');
+    window.localStorage.removeItem('turn');
+} 
